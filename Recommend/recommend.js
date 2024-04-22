996d4158671650b21f004b5c70d23a38ea5878bb
ChrisB555 <108221534+ChrisB555@users.noreply.github.com>
2024-04-22 11:02:02 +0300

    inca in lucru recommend

--- Recommend/recommend.js
+++ Recommend/recommend.js
@@ -1,6 +1,7 @@
 const display = document.querySelector("#display");
 const randomDisplay = document.querySelector("#randomDisplay");
 const error = document.querySelector("#error");
+const refreshBtn = document.querySelector("#refresh");
 const dark = document.querySelector(".dark");
 const recommendText = document.querySelector("#recommendText");
 const randomText = document.querySelector("#randomText");
@@ -9,15 +10,17 @@ let burger = document.querySelector("#burger");
 let closeBtn = document.querySelector("#close");
 let responsiveDrop = document.querySelector("#responsive-drop");
 
-let filme,p,random;
-let idFilmArr;
+let filme, p, random,lastIdRecommended,idRecommended;
+let idFilmArr1, idFilme, idFilmArr3;
 let categoryArr = ["adventure", "action", "comedy", "drama", "thriller"];
 let counts = {};
+let idArr = [];
 
 let response = JSON.parse(localStorage.getItem("arrayFilm"));
-console.log(response);
+console.log("category",response);
+let idFilmArr2 = JSON.parse(localStorage.getItem("idFilmeAfterClick"));
+console.log("idAfterClick",idFilmArr2);
 
-//localStorage.clear("arrayFilm");
 
 const getMovies = async () => {
   const filmeLocal = await fetch("http://localhost:3001/filme/").then(
@@ -29,10 +32,11 @@ const getMovies = async () => {
 const main = async () => {
   const date = await getMovies();
   filme = date;
-  if(response.length === 0){
-  random =  filme[Math.floor(Math.random() * 10)];
-  console.log("server movies", random);
-    p = `<div id = "lista-filme" > 
+  if (response === null) {
+    random = filme[Math.floor(Math.random() * 10)];
+    console.log("server movies", random);
+    randomText.innerHTML = "We can recommend you:";
+    p = `<div id = "lista-filme" >  
     <img class="main-img"src=${random.photo}/> 
     <h3 id="h4">${random.name}</h3> 
     <h5>Director: ${random.director}</h5> 
@@ -41,51 +45,92 @@ const main = async () => {
     <h5>Category: ${random.category} </h5>
     </div>`;
     display.innerHTML += p;
-    console.log(random.id,response);
-    idFilmArr = localStorage.setItem("IdFilme",random.id);
-  }else{
-    filme.filter(e => {
-      if(mostFrequent === e.category){
-        p = `<div id = "lista-filme" > 
-        <img class="main-img"src=${e.photo}/> 
-        <h3 id="h4">${e.name}</h3> 
-        <h5>Director: ${e.director}</h5> 
-        <h5>Release year: ${e.year}</h5> 
-        <h5>Ratings: ${e.rating} </h5>
-        <h5>Category: ${e.category} </h5>
-        </div>`;
-        display.innerHTML += p;
+    console.log(random.id);
+    idFilmArr1 = localStorage.setItem("IdFilmeRandom", random.id);
+  } else {  
+    filme.find(el => { 
+
+      let lastId = idFilmArr2[idFilmArr2.length - 1] ;
+
+       if(el.id.toString() !== lastId && el.category === mostFrequent){     
+      recommendText.innerHTML = "We can recommend you:";
+      p = `<div id = "lista-filme" > 
+          <img class="main-img"src=${el.photo}/> 
+          <h3 id="h4">${el.name}</h3> 
+          <h5>Director: ${el.director}</h5> 
+          <h5>Release year: ${el.year}</h5> 
+          <h5>Ratings: ${el.rating} </h5>
+          <h5>Category: ${el.category} </h5>
+          </div>`;
+      display.innerHTML += p;
+      error.style.display = "none";
+
+      lastIdRecommended = el.id.toString() ;
+      idRecommended = localStorage.getItem("idFilme", idArr);
+      if (idRecommended) {
+       idArr = JSON.parse(idRecommended);
       }
+      idArr.push(lastIdRecommended);
+      localStorage.setItem("idFilme", JSON.stringify(idArr));
+      idFilmArr3 = JSON.parse(localStorage.getItem("idFilme"));
+      console.log("idFilme",idFilmArr3);
+      let lastIdRec = idFilmArr3[idFilmArr3.length - 1];
+
+      idFilmArr3.forEach( el => {
+        if(lastIdRec === el ) {
+          filme.find(el => {
+            if(el.id.toString() !== lastId && lastIdRec !== el.id && el.category === mostFrequent){ 
+              console.log(lastIdRec) 
+            recommendText.innerHTML = "We can recommend you:";
+            p = `<div id = "lista-filme" > 
+                <img class="main-img"src=${el.photo}/> 
+                <h3 id="h4">${el.name}</h3> 
+                <h5>Director: ${el.director}</h5> 
+                <h5>Release year: ${el.year}</h5> 
+                <h5>Ratings: ${el.rating} </h5>
+                <h5>Category: ${el.category} </h5>
+                </div>`;
+           
+        display.innerHTML += p;
+        error.style.display = "none";
+            }else error.innerHTML = "We don't have any movies to recommend you:";
+          })
+         
+        }
+      })
+      return el.category === mostFrequent && el.id.toString() !== lastId;
+
+       } else error.innerHTML = "We don't have any movies to recommend you:";
     })
-  }
 }
+};
 
 main();
 
-  response.forEach(e => {
-      if (counts[e]) {
-          counts[e]++;
-      } else {
-          counts[e] = 1;
-      }
-  });
+response.forEach((e) => {
+  if (counts[e]) {
+    counts[e]++;
+  } else {
+    counts[e] = 1;
+  }
+});
 
- console.log(counts)
+console.log(counts);
 
 let maxCount = 0;
 let mostFrequent = null;
 
-    for (const category in counts) {
-        if (counts[category] > maxCount) {
-            maxCount = counts[category];
-            mostFrequent = category;
-        }
-      
-    } 
-    console.log(mostFrequent);
+for (const category in counts) {
+  if (counts[category] > maxCount) {
+    maxCount = counts[category];
+    mostFrequent = category;
+  }
+}
 
-
-
+const refresh = refreshBtn.addEventListener("click", () => {
+  localStorage.clear("arrayFilm","idFilmeAfterClick","IdFilmeRandom","idFilme");
+  location.reload();
+});
 dark.addEventListener("click", () => {
   let b = document.body;
   b.classList.toggle("dark-mode");
